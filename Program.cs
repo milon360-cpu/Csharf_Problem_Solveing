a26db87d6aa72a7cd2ec8b598e6cc9063c58d8c8
Milon Mondal(Tapu) <76788255+milon360-cpu@users.noreply.github.com>
2023-05-17 01:52:25 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace demo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+         
+            
+
+         //wait for console 
+         Console.ReadKey();
+        }
+    }
+}
